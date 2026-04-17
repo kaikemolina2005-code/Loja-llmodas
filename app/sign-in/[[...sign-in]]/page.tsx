7574ce8cf4f4d08a99c945f1cc48806a116f0c22
kaike <kaikemolina2005@gmail.com>
2026-04-17 16:38:36 -0300

    fix: forçar redirect Clerk para dominio oficial llmodas.shop

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
 
+const OFFICIAL_DOMAIN = "https://llmodas.shop";
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <SignIn />
+      <SignIn
+        forceRedirectUrl={`${OFFICIAL_DOMAIN}/minha-conta`}
+        fallbackRedirectUrl={`${OFFICIAL_DOMAIN}/minha-conta`}
+      />
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,14 @@
 import { SignUp } from "@clerk/nextjs";
 
+const OFFICIAL_DOMAIN = "https://llmodas.shop";
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <SignUp />
+      <SignUp
+        forceRedirectUrl={`${OFFICIAL_DOMAIN}/minha-conta`}
+        fallbackRedirectUrl={`${OFFICIAL_DOMAIN}/minha-conta`}
+      />
     </div>
   );
 }
